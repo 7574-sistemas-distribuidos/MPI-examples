3a8ae10c91eb95ca13cae91043a2eb6595048573
ezetowers <ezequiel.torresfeyuk@gmail.com>
2018-10-11 11:12:49 -0300

    Add Scatter Example

---
+++ Scatter/src/mpi_scatter.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
+#include <stdint.h>
+
+template <int32_t SIZE>
+void data_generation(int32_t rank, double* & rand_array) {
+    if (rank == 0) {
+        for (int32_t i = 0; i < SIZE; ++i) {
+            rand_array[i] = rand() / (RAND_MAX + 1.0); 
+        }
+    }
+}
+
+int32_t dart_computation(const double* rand_array, const int32_t size) {
+    int32_t result = 0;
+    double x = 0.0;
+    double y = 0.0;
+
+    for (int32_t i = 0; i < size; i+=2) {
+        x = rand_array[i];
+        y = rand_array[i+1];
+        if (x*x + y*y < 1.0) {
+            ++result;
+        }
+    }
+    return result;
+}
+
+
+int main (int argc, char* argv[]) {    
+    // Initialize MPI (particularly the amount of processes)
+    MPI_Init(&argc, &argv);
+    
+    //Get process ID
+    int rank = 0;
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    //Get processes Number
+    int size = 0;
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+
+    // Number of processes doing some computation
+    int processes = size - 1;
+
+    // Start 
+    srand((int)time(0));
+    printf("[RANK %d] Starting simulation\n", rank);
+
+    // Generate a huge array of random numbers JUST in rank 0 process
+    // MAX_INT:  2147483647
+    const int32_t N = 10000000;
+    const int32_t N_PER_PROCESS = N / processes;
+    double* rand_array = NULL;
+    double* scatter_rand_array = NULL;
+    scatter_rand_array = new double[N_PER_PROCESS];
+
+    if (rank == 0) {
+        rand_array = new double[N];
+        data_generation<N>(rank, rand_array);
+
+        printf("[RANK %d] Proceed to send data\n", rank);
+        MPI_Scatter(rand_array, N_PER_PROCESS, MPI_DOUBLE, scatter_rand_array, N_PER_PROCESS, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        printf("[RANK %d] Data has been sent\n", rank);
+
+        int32_t cum_results = 0;
+        int32_t remote_result = 0;
+        // Receive the hits from all the processes
+        for (int32_t i = 1; i < size; ++i) {
+            MPI_Recv(&remote_result, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("[RANK %d] Result received from Process %d. Result: %d\n", rank, i, remote_result);
+            cum_results += remote_result;
+        }
+
+        double pi_estimation = 4 * cum_results / (double) (N/2);
+        printf("[RANK %d] All results have arrived. Pi estmation: %0.12f", pi_estimation);
+
+        delete [] rand_array;
+    }
+    else {
+        // scatter_rand_array = new double[N_PER_PROCESS];
+        printf("[RANK %d] Proceed to receive data\n", rank);
+        MPI_Scatter(rand_array, N_PER_PROCESS, MPI_DOUBLE, scatter_rand_array, N_PER_PROCESS, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        printf("[RANK %d] Data has been received\n", rank);
+
+        // Execute the computation
+        int32_t result = dart_computation(scatter_rand_array, N_PER_PROCESS);
+
+        printf("[RANK %d] Proceed to send result to root: %d\n", rank, result);
+        MPI_Send(&result, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        delete [] scatter_rand_array;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
